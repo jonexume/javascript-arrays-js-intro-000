52507bf1b6b2d7e449b6baca231db26967e1f632
Jon Exume <jonexume@gmail.com>
2018-02-04 03:41:24 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,51 @@
+// JavaScript Arrays
+
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element) {
+  chocolateBars.unshift("M&M");
+  console.log(chocolateBars);
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  chocolateBars = ["100grand", ...chocolateBars];
+  return chocolateBars;
+}
+
+function addElementToEndOfArray() {
+  chocolateBars.push("almondjoy");
+  return chocolateBars;
+}
+
+function destructivelyAddElementToEndOfArray() {
+  var fruits = ["apples", "oranges", "bananas", "tomatoes"]
+  fruits.push("tangerines")
+  return fruits
+}
+
+function accessElementInArray() {
+
+  return chocolateBars[2];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray() {
+
+  chocolateBars.shift();
+  return chocolateBars;
+}
+
+function removeElementFromBeginningOfArray() {
+  chocolateBars.slice();
+  return chocolateBars;
+}
+
+function destructivelyRemoveElementFromEndOfArray() {
+
+  chocolateBars.slice(-1);
+  return chocolateBars;
+}
+
+function removeElementFromEndOfArray() {
+  chocolateBars.slice(0, chocolateBars.length -1);
+  return chocolateBars;
+}
