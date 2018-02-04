0f9bb26a3758bccdb4a26876238969b2cc0c81a8
Jon Exume <jonexume@gmail.com>
2018-02-04 06:12:29 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -35,7 +35,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array, element) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(-2);
+  array.slice(1);
   return array;
 }
 
